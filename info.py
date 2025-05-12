0bf29ab548907f2a00050ea0418bb347f73179b1
HDMoviesEarth <diesengaming517@gmail.com>
2025-05-12 12:28:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1900118264').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/UHD_NETWORK_HelpBot") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/ANKAN_BEST_74") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002318862336'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+Q4igxofLeKlhOGM1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002288558945').split()]
